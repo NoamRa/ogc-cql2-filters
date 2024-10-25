c72a8e4c422e9eea956bfd3f29a1e596ee6e4604
NoamR <noam.raby@up42.com>
2024-10-25 14:20:05 +0200

    add playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
+import { useState } from "react";
+import { run } from "./logic/run";
+import Result from "./components/Result";
+import Code from "./components/Code";
+
 export function App() {
+  const [filter, setFilter] = useState(expressionExamples[0]);
+  const ast = run(filter, { inputType: "text" });
+
   return (
-    <main>
-      <h1>OGC CQL2 Filters</h1>
-    </main>
+    <div style={{ width: "800px", margin: "auto" }}>
+      <main>
+        <h1>OGC CQL2 Filters playground</h1>
+        <section>
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+            <h2>CQL2 text</h2>
+            <select
+              onChange={(evt) => {
+                setFilter(evt.target.value);
+              }}
+            >
+              {expressionExamples.map((example, index) => (
+                <option key={index}>{example}</option>
+              ))}
+            </select>
+          </div>
+          <textarea
+            value={filter}
+            onChange={(evt) => {
+              setFilter(evt.target.value);
+            }}
+            style={{ width: "99%" }}
+            rows={8}
+          />
+        </section>
+        <section>
+          <h2>Results:</h2>
+          <div style={{ display: "flex", flexDirection: "row", gap: "16px" }}>
+            <Result title="Text">
+              <Code>{ast.toString()}</Code>
+            </Result>
+            <Result title="JSON">
+              <Code>{JSON.stringify(ast.toJSON(), null, 2)}</Code>
+            </Result>
+          </div>
+        </section>
+      </main>
+      <hr />
+      <footer>
+        Open Geospatial Consortium (GCQ) Common Query Language (CQL2) filter builder
+        <br />
+        <a href="https://www.ogc.org/standard/cql2/" target="_blank" rel="noreferrer noopener">
+          Common Query Language (CQL2) Standard page
+        </a>
+        &nbsp;&bull;&nbsp;
+        <a href="https://www.opengis.net/doc/IS/cql2/1.0" target="_blank" rel="noreferrer noopener">
+          The standard itself
+        </a>
+        <br />
+        <a href="https://github.com/NoamRa/ogc-cql2-filters" target="_blank" rel="noreferrer noopener">
+          Source on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
+
+const expressionExamples = ["vehicle_height > (bridge_clearance-1)"];
---
+++ src/components/Result.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+
+export default function Result({ title, children }: PropsWithChildren<{ title: string }>) {
+  return (
+    <div style={{ flex: 1 }}>
+      <h3>{title}</h3>
+      <div style={{ border: "1px var(--text) solid" }}>{children}</div>
+    </div>
+  );
+}

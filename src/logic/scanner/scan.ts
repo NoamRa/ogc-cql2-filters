198faba4e24d104e83135c50cd7ae4fe1c57f0f5
NoamR <noam.raby@up42.com>
2024-10-25 14:19:52 +0200

    fix ts-lint / eslint

--- src/logic/scanner/scan.ts
+++ src/logic/scanner/scan.ts
@@ -5,12 +5,11 @@ import scanText from "./scanText";
 
 export default function scan(input: string, options: RunOptions) {
   if (options.inputType === "text") return scanText(input);
-  if (options.inputType === "JSON") {
-    try {
-      return scanJSON(JSON.parse(input));
-    } catch (error) {
-      throw new ScanError(`Failed to parse JSON input: ${(error as SyntaxError)?.message}.`);
-    }
+
+  try {
+    return scanJSON(JSON.parse(input) as object);
+  } catch (error) {
+    throw new ScanError(`Failed to parse JSON input: ${(error as SyntaxError).message}.`);
   }
 
   throw new ScanError("Invalid input type option");

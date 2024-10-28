36edb2d9e7a70ab94e628a9d6063130bd89e6061
NoamR <noam.raby@up42.com>
2024-10-28 11:22:36 +0100

    update scan

--- src/logic/scanner/scan.ts
+++ src/logic/scanner/scan.ts
@@ -4,13 +4,19 @@ import scanJSON from "./scanJSON";
 import scanText from "./scanText";
 
 export default function scan(input: string, options: RunOptions) {
-  if (options.inputType === "text") return scanText(input);
-
-  try {
-    return scanJSON(JSON.parse(input) as object);
-  } catch (error) {
-    throw new ScanError(`Failed to parse JSON input: ${(error as SyntaxError).message}.`);
+  switch (options.inputType) {
+    case "text": {
+      return scanText(input);
+    }
+    case "JSON": {
+      try {
+        return scanJSON(JSON.parse(input) as object);
+      } catch (error) {
+        throw new ScanError(`Failed to parse JSON input: ${(error as SyntaxError).message}.`);
+      }
+    }
+    default: {
+      throw new ScanError("Invalid input type option");
+    }
   }
-
-  throw new ScanError("Invalid input type option");
 }

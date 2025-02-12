67e12282698b1590b3e176dd3cf65e1e51ae5387
NoamR <noamraby@gmail.com>
2025-02-12 16:08:30 +0100

    basic ability to parse in CLI

--- src/logic/parse.ts
+++ src/logic/parse.ts
@@ -3,28 +3,24 @@ import { parseJSON } from "./parser/parseJSON";
 import { parseText } from "./parser/parseText";
 import { scanText } from "./scanner/scanText";
 
-type InputType = string | object;
-
-export function parse(input: InputType): Expression {
-  try {
-    if (typeof input === "object") {
-      return parseJSON(input);
-    }
-    if (typeof input === "string") {
-      if (input.startsWith("{")) {
-        return parseJSON(JSON.parse(input) as object);
-      }
-      return parseText(scanText(input));
-    }
-
-    // failed to detect encoding
-    throw new Error("Failed to detect input type");
-  } catch (error) {
-    const message = error instanceof Error ? error.message : "Failed to parse input";
-    return {
-      toText: () => message,
-      toJSON: () => message,
-      accept: () => message,
-    } as Expression;
+/**
+ * Generic CQL2 parse function
+ * Includes basic heuristic to determine input's encoding
+ * @param {string | object} input
+ * @returns {Expression} Expression AST
+ * @throws {ScanError | ParseJSONError | ParseTextError} Error
+ */
+export function parse(input: string | object): Expression {
+  if (typeof input === "object") {
+    return parseJSON(input);
   }
+  if (typeof input === "string") {
+    if (input.startsWith("{")) {
+      return parseJSON(JSON.parse(input) as object);
+    }
+    return parseText(scanText(input));
+  }
+
+  // Failed to detect encoding
+  throw new Error("Failed to detect input type");
 }

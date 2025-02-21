8b8dad0e96c42c173b2bb92a40e61b3e6638d34c
NoamR <noamraby@gmail.com>
2025-02-21 11:38:39 +0100

    test case insensitive and accent insensitive operators

--- src/cql/Entities/Expression.test.ts
+++ src/cql/Entities/Expression.test.ts
@@ -292,6 +292,7 @@ describe("Test Expressions", () => {
       expect(isNullExpr.text).toBe("is null");
       expect(isNullExpr.json).toBe("isNull");
       expect(isNullExpr.label).toBe("is null");
+      expect(isNullExpr.arity).toBe(Arity.Unary);
       expect(isNullExpr.textFormatter).toBeTypeOf("function");
 
       const isNotNullExpr = new Expressions.IsNullOperatorExpression(expr, true);
